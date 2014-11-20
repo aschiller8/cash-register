53bf711e8a78f1a316b0738d965da73e64edc8ec
aschiller8 <aschiller8@hotmail.com>
2014-11-20 10:07:38 -0500

    added car functions

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 "use strict"; // This line chooses a JavaScript dialect, one that helps both jsLint (used in OrionHub) and browsers catch errors.
 /*jslint browser: true*/ // This line tells jsLint that the code will run in a browser.
 
+
+var total;
+var subtotal;
+var tax;
+var running_subtotal, running_total, running_tax;
+
 function cash_register(item) {
-	var total;
+	var subtotal, total, tax;
+	subtotal = prices[item];
+	tax = subtotal * .15;
+	total = subtotal + tax;
 	
 	
 }
@@ -17,3 +26,38 @@ document.getElementById('Jaguar').addEventListener('click',Jaguar);
 document.getElementById('Porsche').addEventListener('click',Porsche);
 document.createElement('Mini_Coup').addEventListener('click',Mini_Coup);
 
+function Benz() {
+	cash_register('Benz');
+}
+
+function MiniVan() {
+	cash_register('Mini_Van');
+}
+
+function mustang(){
+	cash_register('Mustang');
+}
+
+function lambo(){
+	cash_register('Lambo');
+}
+
+function ram1500(){
+	cash_register('Ram_1500');
+}
+
+function fordf150(){
+	cash_register('Ford_F150');
+}
+
+function jaguar(){
+	cash_register('Jaguar');
+}
+
+function porsche(){
+	cash_register('Porsche');
+}
+
+function minicoup(){
+	cash_register('Mini_Coup');
+}
